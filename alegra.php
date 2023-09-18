b868711d91446c80b2c989a0ef6bff981f49b6f7
Alex Monroy <alexwebpuro@gmail.com>
2023-09-18 18:57:12 -0500

    Initial commit!

---
+++ alegra.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * Plugin Name:       Alegra facturacion
+ * Plugin URI:        https://alexmonroy.com/plugins/alegra/
+ * Description:       Crea tus facturas desde woocommerce.
+ * Version:           1.10.3
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Alex Monroy
+ * Author URI:        https://alexmonroy.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://alexmonroy.com/alegra/
+ * Text Domain:       alegra
+ * Domain Path:       /languages
+ */
