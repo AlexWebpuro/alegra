c545e72e136d9df98df86d34d4f03d0127cc9d2f
Alex Monroy <alexwebpuro@gmail.com>
2023-09-27 13:37:56 -0500

    Refact: Add admin page and create a class to handle all this admin pages.

--- includes/Pages/Admin.php
+++ includes/Pages/Admin.php
@@ -2,18 +2,32 @@
 
 namespace Alex\Alegra\Pages;
 use \Alex\Alegra\Base\BaseController;
+use \Alex\Alegra\Api\SettingsApi;
 
 class Admin extends BaseController
 {
+	public $settings;
+	public $pages;
+
+	public function __construct()
+	{
+		$this->settings = new SettingsApi();
+		$this->pages = $pages = array(
+			array(
+				'page_title' => 'alegra',
+				'menu_title' => 'Alegra',
+				'capability' => 'manage_options',
+				'menu_slug' => 'alegra',
+				'callback' => function() {
+					echo '<h1>Alex</h1>';
+				},
+				'icon_url' => 'dashicons-plugins-checked',
+				'position' => 5 
+			),
+		);
+	}
+
     public function register() {
-        add_action('admin_menu', array($this, 'add_admin_page'), 10, 0);
+		$this->settings->AddPages( $this->pages )->register();
     }
-
-	public function add_admin_page() {
-		add_menu_page('alegra','Alegra','manage_options','alegra', array( $this, 'admin_index'),'dashicons-plugins-checked',5 );
-	}
-
-	public function admin_index() {
-		require_once $this->plugin_path . 'templates/admin.php';
-	}
 }
