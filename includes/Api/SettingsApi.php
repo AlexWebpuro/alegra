b841adaba04a8b80342278b70af66a32e0b5e7a4
Alex Monroy <alexwebpuro@gmail.com>
2023-09-27 16:01:01 -0500

    Add: Submenu and subpages with class contruct.

--- includes/Api/SettingsApi.php
+++ includes/Api/SettingsApi.php
@@ -5,6 +5,8 @@ namespace Alex\Alegra\Api;
 class SettingsApi
 {
     public $admin_pages = array();
+    public $admin_subpages = array();
+    public $subpages = array();
 
     public function register() {
         if( ! empty( $this->admin_pages ) ) {
@@ -14,6 +16,38 @@ class SettingsApi
 
     public function AddPages( array $pages ) {
         $this->admin_pages = $pages;
+        return $this;
+    }
+
+    public function withSubPage( string $title = null ) {
+        if( empty( $this->admin_pages) ) {
+            return $this;
+        }
+
+        $admin_page = $this->admin_pages[0];
+
+        $subpages = array(
+            array(
+                'parent_slug'   => $admin_page['menu_slug'],
+                'page_title'    => $admin_page['page_title'],
+				'menu_title'    => ( $title ) ? $title : $admin_page['menu_title'],
+				'capability'    => $admin_page['capability'],
+				'menu_slug'     => $admin_page['menu_slug'],
+				'callback'      => $admin_page['callback'],
+            ),
+        );
+
+        $this->admin_subpages = $subpages;
+
+        // dd( $subpages );
+
+        return $this;
+
+    }
+
+    public function addSubPages( array $pages ) {
+        $this->admin_subpages = array_merge( $this->admin_subpages, $pages);
+
         return $this;
     }
 
@@ -27,6 +61,17 @@ class SettingsApi
                 $page['callback'],
                 $page['icon_url'],
                 $page['position']
+            );
+        }
+        // dd( $this->admin_subpages );
+        foreach( $this->admin_subpages as $page ) {
+            add_submenu_page( 
+                $page['parent_slug'],
+                $page['page_title'],
+                $page['menu_title'],
+                $page['capability'],
+                $page['menu_slug'],
+                $page['callback'],
             );
         }
     }
--- includes/Pages/Admin.php
+++ includes/Pages/Admin.php
@@ -7,7 +7,8 @@ use \Alex\Alegra\Api\SettingsApi;
 class Admin extends BaseController
 {
 	public $settings;
-	public $pages;
+	public $pages = array();
+	public $subpages = array();
 
 	public function __construct()
 	{
@@ -25,9 +26,23 @@ class Admin extends BaseController
 				'position' => 5 
 			),
 		);
+
+		$this->subpages =  array(
+            array(
+                'parent_slug' => 'alegra',
+                'page_title' => 'Settings',
+				'menu_title' => 'Settings',
+				'capability' => 'manage_options',
+				'menu_slug' => 'alegra-settings',
+				'callback' => function() {
+					echo '<h1>Other tab for my plugin!</h1>';
+				},
+            ),
+        );
+		// dd( $this->subpages );
 	}
 
     public function register() {
-		$this->settings->AddPages( $this->pages )->register();
+		$this->settings->AddPages( $this->pages )->withSubPage( 'Dashboard' )->addSubPages( $this->subpages )->register();
     }
 }
