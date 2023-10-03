282559140bffed30a7750474f6d9e2e53a0db913
Alex Monroy <alexwebpuro@gmail.com>
2023-10-03 17:30:17 -0500

    Add: Create contact in Alegra URL ( Costumer ).

--- includes/AlegraAPI/BaseController.php
+++ includes/AlegraAPI/BaseController.php
@@ -5,8 +5,10 @@ namespace Alex\Alegra\AlegraAPI;
 class BaseController
 {
     public $contact;
+    public $crete_contact;
 
     public function __construct() {
         $this->contact = 'https://api.alegra.com/api/v1/';
+        $this->crete_contact = 'https://api.alegra.com/api/v1/contacts';
     }    
 }
