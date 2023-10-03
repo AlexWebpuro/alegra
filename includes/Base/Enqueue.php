9b8cdf52151ba4026c9da471887fcd7e0af2b8d1
Alex Monroy <alexwebpuro@gmail.com>
2023-10-03 17:31:05 -0500

    Add: Ajax handle TEST API Alegra.

--- includes/Base/Enqueue.php
+++ includes/Base/Enqueue.php
@@ -8,21 +8,19 @@ class Enqueue extends BaseController
 {
     public function register() {
         add_action('admin_enqueue_scripts', array( $this, 'enqueue'));
-		add_action( 'wp_ajax_alegra_test_connection', function() {
-			$alegra = new Alegra;
-
-			$alegra->getCretentials();
-			$test = $alegra->testAPIConnection();
-
-			// echo "<pre> $test </pre>";
-			echo "$test";
-			wp_die();
-		});
-
+		add_action( 'wp_ajax_alegra_test_connection', array( $this, 'loadTestConnection' ) );
     }
-
+	
 	function enqueue() {
 		wp_enqueue_script('alegra', $this->plugin_url . '/assets/main.js', array(), '1.0', true );
 		wp_enqueue_style('alegra', $this->plugin_url . 'assets/style.css', array(), '1.0', 'all' );
+	}
+	
+	function loadTestConnection() {
+		$alegra = new Alegra;
+		$alegra->getCretentials();
+		$response = $alegra->testAPIConnection();
+		echo $response;
+		wp_die();
 	}
 }
