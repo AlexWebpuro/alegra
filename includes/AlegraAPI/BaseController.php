c921e70ed3db38b271d0c7e8fd82ab6d5778721e
Alex Monroy <alexwebpuro@gmail.com>
2023-10-10 11:54:11 -0500

    Update: Includes all endpoints and API Alegra information.

--- includes/AlegraAPI/BaseController.php
+++ includes/AlegraAPI/BaseController.php
@@ -4,11 +4,25 @@ namespace Alex\Alegra\AlegraAPI;
 
 class BaseController
 {
+    public $user;
+    
+    public $token;
+
+    protected $hash;
+
     public $contact;
-    public $crete_contact;
+
+    public $create_contact;
+
+    public $item;
+
 
     public function __construct() {
+        $this->user = get_option( 'alegra_user' );
+        $this->token = get_option( 'alegra_pass' );
+        $this->hash = "Basic " . base64_encode( "$this->user:$this->token");
         $this->contact = 'https://api.alegra.com/api/v1/';
-        $this->crete_contact = 'https://api.alegra.com/api/v1/contacts';
-    }    
+        $this->create_contact = 'https://api.alegra.com/api/v1/contacts';
+        $this->item = "https://api.alegra.com/api/v1/items";
+    }
 }
